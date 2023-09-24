e7ca64a2b4607440005111268af630465f88ad45
Md Sohag Biswas <95488340+GodMischief@users.noreply.github.com>
2023-09-24 13:48:39 +0530

    Update Percentage_Calculator.java

--- Percentage_Calculator.java
+++ Percentage_Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Percentage_Calculator{
+public class Percentage_Calculator {
     public static void main(String[] args) {
         System.out.println("Grade Card");
         Scanner sc = new Scanner(System.in);
